f8e03067637b300fda4701f69848f37f8ccffcbd
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2011-02-09 07:19:33 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/googlemapbasic/trunk@2904 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- javascript/GoogleMapBasic.js
+++ javascript/GoogleMapBasic.js
@@ -6,17 +6,6 @@ var GoogleMapBasicAddress = '';
 */
 
 
-/*
-SOME IDEAS FOR ADDITIONAL OPTIONS - see google map API for 1000000 more options
-	var baseIcon = new GIcon(G_DEFAULT_ICON);
-	baseIcon.iconSize = new GSize(60, 46);
-	baseIcon.shadowSize = new GSize(60, 46);
-	baseIcon.iconAnchor = new GPoint(18, 46);
-	baseIcon.infoWindowAnchor = new GPoint(9, 2);
-	baseIcon.image = "/themes/main/images/mycompany-icon.png";
-	baseIcon.shadow = "/themes/main/images/mycompany-icon-shadow.png";
-*/
-
 function GoogleMapBasicInit() {
 	if (GBrowserIsCompatible()) {
 		var map = new GMap2(document.getElementById("GoogleMapBasic"));
@@ -31,15 +20,7 @@ function GoogleMapBasicInit() {
 				}
 				else {
 					map.setCenter(point, 15);
-					/* ICON ...
-					var myIcon = new GIcon(); //can add baseIcon here new GIcon(baseIcon);
-					markerOptions = { icon:myIcon};
-					var marker = new GMarker(point, markerOptions);
-					*/
-					/* NO ICON */
-					GEvent.addListener(marker, "click", function() {
-						marker.openInfoWindowHtml(point, GoogleMapBasicInfoWindow);
-					});
+					var marker = createMarker(point, GoogleMapBasicInfoWindow);
 					map.addOverlay(marker);
 				}
 			}
