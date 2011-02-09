b69ab9f9afd99d60a28cc1f4284eb175ff05642b
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2011-02-09 05:55:32 +0000

    added first version of GoogleMapBasic

---
+++ _config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * developed by www.sunnysideup.co.nz
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+**/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START googlemapbasic MODULE ----------------===================
+//MUST SET
+//Object::add_extension('SiteTree', 'GoogleMapBasic');
+//Object::add_extension('ContentController', 'GoogleMapBasic_Controller');
+//GoogleMapBasic::set_key("abcdef");
+
+//MAY SET
+//GoogleMapBasic::set_js_location("mysite/javascript/MyMap.js");
+//GoogleMapBasic::set_exclude_from_classes(array("HomePage"));
+//GoogleMapBasic::set_include_in_classes(array("ContactUsPage"));
+//===================---------------- END googlemapbasic MODULE ----------------===================
+
+
+
+
+
