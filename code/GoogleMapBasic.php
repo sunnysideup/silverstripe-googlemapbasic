3b1482f8d70bd41795627a35955f5f3528b73a94
romain <romain@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2011-05-19 22:43:30 +0000

    GM requirements moved to static function

--- code/GoogleMapBasic.php
+++ code/GoogleMapBasic.php
@@ -70,17 +70,21 @@ class GoogleMapBasic extends DataObjectDecorator {
 }
 
 class GoogleMapBasic_Controller extends Extension {
-
+	
+	static function add_requirements() {
+		$fileLocation = GoogleMapBasic::get_js_location();
+		if(! $fileLocation) {
+			$fileLocation = 'googlemapbasic/javascript/GoogleMapBasic.js';
+		}
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript('http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=false&amp;key=' . GoogleMapBasic::get_key());
+		Requirements::javascript($fileLocation);
+	}
+	
 	function GoogleMapBasic() {
 		if($this->owner->ShowMap && $this->owner->Address) {
-			$fileLocation = GoogleMapBasic::get_js_location();
-			if(!$fileLocation) {
-				$fileLocation = 'googlemapbasic/javascript/GoogleMapBasic.js';
-			}
+			self::add_requirements();
 			$infoWindow = '<div id="InfoWindowContent">'.$this->owner->InfoWindowContent.'</div>';
-			Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
-			Requirements::javascript('http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=false&amp;key='.GoogleMapBasic::get_key());
-			Requirements::javascript($fileLocation);
 			Requirements::customScript("GoogleMapBasic.infoWindow = ( \"".$this->cleanJS($infoWindow)."\")", 'GoogleMapBasicInfoWindow');
 			Requirements::customScript("GoogleMapBasic.address = (\"".$this->cleanJS($this->owner->Address)."\")", 'GoogleMapBasicAddress');
 			Requirements::customScript("GoogleMapBasic.zoomLevel = (".intval($this->owner->ZoomLevel).")", 'GoogleMapBasicZoomLevel');
