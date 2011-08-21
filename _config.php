6031d4f13028903e32383c01ffe3d56350345730
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2011-08-21 01:49:09 +0000

    allowing multiple keys to be added to googlemap basic (for live / dev / test) site

--- _config.php
+++ _config.php
@@ -5,13 +5,15 @@
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
 
-
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START googlemapbasic MODULE ----------------===================
 //MUST SET
 //Object::add_extension('SiteTree', 'GoogleMapBasic');
 //Object::add_extension('ContentController', 'GoogleMapBasic_Controller');
+//DEFAULT
 //GoogleMapBasic::set_key("abcdef"); //see http://code.google.com/apis/maps/signup.html
+//OTHER SITES
+//GoogleMapBasic::add_key("abcdef", $url = "http://www.mysite.com"); //see http://code.google.com/apis/maps/signup.html
 
 //MAY SET
 //GoogleMapBasic::set_js_location("mysite/javascript/MyMap.js"); //customise JS
