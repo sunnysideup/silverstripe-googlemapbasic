8e81ef5b6a8db6ca0022b0c03ccebe5272b9e631
nt@rainbox <nt@sunnysideup.co.nz>
2026-08-24 12:15:09 +1200

    FIX: make sure dom is loaded before loading map

--- client/javascript/GoogleMapBasic.js
+++ client/javascript/GoogleMapBasic.js
@@ -1,58 +1,45 @@
-/*
-THANK YOU Marcel Nogueira d' Eurydice FOR THE INSPIRATION!
-*/
-
 window.kickstartGoogleMaps = () => {
-  window.setTimeout(() => {
-    if (typeof window.GoogleMapBasicOptions !== 'undefined') {
-      for (let i = 0; i < window.GoogleMapBasicOptions.length; i++) {
-        let options = window.GoogleMapBasicOptions[i]
-        var map = new GoogleMapBasic(options)
-        for (let key in options) {
-          if (!options.hasOwnProperty(key)) {
-            // The current property is not a direct property of p
-            continue
-          }
-          map.setVar(key, options[key])
-          //Do your logic with the property here
-        }
-        map.init()
+  if (typeof window.GoogleMapBasicOptions === 'undefined') {
+    return
+  }
+  for (let i = 0; i < window.GoogleMapBasicOptions.length; i++) {
+    let options = window.GoogleMapBasicOptions[i]
+    var map = new GoogleMapBasic(options)
+    for (let key in options) {
+      if (!options.hasOwnProperty(key)) {
+        continue
       }
+      map.setVar(key, options[key])
     }
-  }, 1000)
+    map.init()
+  }
 }
 
 function GoogleMapBasic (options) {
   var mapObject = {
     // to be provided
     idOfMapDiv: options.idOfMapDiv,
-
     zoomLevel: options.zoomLevel,
-
     infoWindowContent: options.infoWindowContent,
-
     address: options.address,
-
     lat: options.lat,
-
     lng: options.lng,
-
     title: options.title,
 
     // internal items
-
     map: null,
-
     mapOptions: null,
-
     marker: null,
-
     infoWindowObject: null,
-
     location: null,
 
     init: function () {
-      if (mapObject.lat && mapObject.lng) {
+      // Note: check against null/undefined, not truthiness, so lat/lng of 0 still work.
+      let hasCoords =
+        mapObject.lat !== null && mapObject.lat !== undefined && mapObject.lat !== 0 &&
+        mapObject.lng !== null && mapObject.lng !== undefined && mapObject.lng !== 0
+
+      if (hasCoords) {
         mapObject.location = { lat: mapObject.lat, lng: mapObject.lng }
         mapObject.createMap()
       } else {
@@ -62,7 +49,6 @@ function GoogleMapBasic (options) {
           function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
               mapObject.location = results[0].geometry.location
-              // we have to do this now after the address is found!
               mapObject.createMap()
             } else {
               alert(
@@ -75,16 +61,22 @@ function GoogleMapBasic (options) {
     },
 
     createMap: function () {
+      // The div may not be in the DOM yet on a warm-cached page load, where
+      // Google's callback can fire before the parser reaches the map element.
+      // Wait for it instead of throwing "Expected mapDiv ... but was passed null".
+      mapObject.waitForElement(mapObject.idOfMapDiv, function (el) {
+        mapObject.buildMap(el)
+      })
+    },
+
+    buildMap: function (el) {
       mapObject.mapOptions = {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         zoom: mapObject.zoomLevel,
         center: mapObject.location,
         scrollwheel: false
       }
-      mapObject.map = new google.maps.Map(
-        document.getElementById(mapObject.idOfMapDiv),
-        mapObject.mapOptions
-      )
+      mapObject.map = new google.maps.Map(el, mapObject.mapOptions)
       mapObject.marker = new google.maps.Marker({
         map: mapObject.map,
         position: mapObject.location,
@@ -97,6 +89,35 @@ function GoogleMapBasic (options) {
         mapObject.infoWindowObject.open(mapObject.map, mapObject.marker)
       })
       google.maps.event.trigger(mapObject.marker, 'click')
+    },
+
+    waitForElement: function (id, callback) {
+      let el = document.getElementById(id)
+      if (el) {
+        callback(el)
+        return
+      }
+
+      let observer = new MutationObserver(function () {
+        let found = document.getElementById(id)
+        if (found) {
+          observer.disconnect()
+          clearTimeout(timeout)
+          callback(found)
+        }
+      })
+      observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      })
+
+      // Safety net so we never observe forever.
+      let timeout = setTimeout(function () {
+        observer.disconnect()
+        console.error(
+          'GoogleMapBasic: element "' + id + '" never appeared in the DOM'
+        )
+      }, 10000)
     }
   }
 
